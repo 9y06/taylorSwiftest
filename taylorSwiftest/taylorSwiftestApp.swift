f1468e1797302a77168e8ddd75afb96395f1ba7c
9y06 <oygnijoes0209@gmail.com>
2024-09-09 19:41:10 +0900

    Initial Commit

---
+++ taylorSwiftest/taylorSwiftestApp.swift
@@ -0,0 +1,17 @@
+//
+//  taylorSwiftestApp.swift
+//  taylorSwiftest
+//
+//  Created by dgsw21 on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct taylorSwiftestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
