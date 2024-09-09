24354f799bec70b994ba593738fd687feccb4b7b
9y06 <oygnijoes0209@gmail.com>
2024-09-09 20:13:23 +0900

    Xcode깃허브커밋테스트입니ㄷ

--- taylorSwiftest/ContentView.swift
+++ taylorSwiftest/ContentView.swift
@@ -10,10 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "bubbles.and.sparkles")
+                    .imageScale(.large)
+                    .foregroundStyle(.mint)
+                    .shadow(color:.blue,radius: 5)
+                Image(systemName: "sparkles")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .shadow(color:.cyan,radius: 5)
+                Image(systemName: "heart.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.white)
+                    .shadow(color:.black,radius: 5)
+                Image(systemName: "swift")
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                    .shadow(color:.pink,radius: 5)
+            }
+            Text("테일러스위프트띵곡을들으며스위프트공부를합시다")
+                .fontWeight(.bold)
         }
         .padding()
     }
